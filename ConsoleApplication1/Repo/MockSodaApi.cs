d6407bf896d92516024bdfc4c5814b2d56234f2a
Mathias <mnssvendsen@gmail.com>
2020-09-22 08:03:53 +0200

    clean up code

--- ConsoleApplication1/Repo/MockSodaApi.cs
+++ ConsoleApplication1/Repo/MockSodaApi.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using ConsoleApplication1.Moddels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace ConsoleApplication1.Repo
 {
-    class MockSodaApi
+    public class MockSodaApi
     {
+        public Soda[] GetSodas()
+        {
+            var inventory = new[] {
+            new Soda { Name = "coke", Nr = 5, Price = 20 },
+            new Soda { Name = "sprite", Nr = 3, Price = 15 },
+            new Soda { Name = "fanta", Nr = 0, Price = 15 }
+        };
+            return inventory;
+        }
     }
 }
